65dc06fff4d6fa437266051bb873c4e9761d1f96
Himanshu Singh <61236944+himanshusingh335@users.noreply.github.com>
2020-10-28 07:27:33 +0530

    http.put partially ready

--- lib/backend/api.dart
+++ lib/backend/api.dart
@@ -96,3 +96,24 @@ Future<Society> fetchAlbum() async {
     throw Exception('Failed to load album 1');
   }
 }
+
+Future<Society> createAlbum(String title) async {
+  final http.Response response = await http.post(
+    'https://json-serialization.firebaseio.com/.json',
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'title': title,
+    }),
+  );
+  if (response.statusCode == 201) {
+    // If the server did return a 201 CREATED response,
+    // then parse the JSON.
+    return Society.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 201 CREATED response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
